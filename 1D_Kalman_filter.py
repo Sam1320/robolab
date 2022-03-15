250fcea2cf397a4e0c4a6d1fe18bf9c6b9620340
Samuel Rodriguez <samrolopez@gmail.com>
2022-03-15 12:42:52 +0100

    store parameters in dictionary and simplify Inputbox class

--- 1D_Kalman_filter.py
+++ 1D_Kalman_filter.py
@@ -28,7 +28,7 @@ plot_width = win_width
 robot_x = random.randint(0, win_width-1) + panel_width
 
 mu = robot_x
-sigma_init = 200
+sigma = 200
 
 ratio = [0.5, 1, 2, 4, 8]
 motion_sigma = 10.
@@ -40,18 +40,20 @@ step_size = 10
 win_height = cell_width+plot_height
 plot_color = 'blue'
 
+params = {'mu': robot_x, 'sigma': sigma, 'measurement_sigma': measurement_sigma, 'motion_sigma': motion_sigma,
+          'step_size': step_size}
 def main():
-    global screen, robot_x, mu, sigma_init, plot_color
+    global screen, robot_x, plot_color, params
     pg.init()
     screen = pg.display.set_mode((panel_width+win_width, cell_width+plot_height))
     screen.fill('black')
 
-    parameters = ['sigma_init', 'measurement_sigma', 'motion_sigma', 'step_size']
-    input_boxes = [InputBox(0, label_height*i, screen=screen, name=name) for i, name in enumerate(parameters)]
+    input_boxes = {name:InputBox(0, label_height*i, screen=screen, name=name, value=params[name], font=font)
+                   for i, name in enumerate(params)}
     move_text = "For motion updates use the arrow keys and use the m key for measurement updates."
-    utils.blit_text(screen, move_text, (0, len(parameters)*label_height+10), width=panel_width, height=win_height, color='white', font=font)
+    utils.blit_text(screen, move_text, (0, len(params)*label_height+10), width=panel_width, height=win_height, color='white', font=font)
     while True:
-        drawgrid2()
+        drawgrid()
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
@@ -59,36 +61,40 @@ def main():
                 sys.exit()
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_LEFT:
-                    action = -1 * step_size
+                    action = -1 * params['step_size']
                 elif event.key == pg.K_RIGHT:
-                    action = 1 * step_size
+                    action = 1 * params['step_size']
                 else:
                     action = False
                 if action:
-                    mu, sigma_init = motion_update(mu, sigma_init, action)
-                    true_move = np.random.normal(action, motion_sigma, 1)[0]
+                    params['mu'], params['sigma'] = motion_update(params['mu'], params['sigma'], action, params['motion_sigma'])
+                    true_move = np.random.normal(action, params['motion_sigma'], 1)[0]
                     robot_x = (robot_x + true_move)
                     # bound movements to grid limits
                     plot_color = 'red'
                     robot_x = max(min(win_width-1+panel_width, robot_x), 0+panel_width)
                 if event.key == pg.K_m:
-                    measurement = np.random.normal(robot_x, measurement_sigma, 1)[0]
+                    measurement = np.random.normal(robot_x, params['measurement_sigma'], 1)[0]
                     print(f'z = {measurement} truth = {robot_x}')
-                    mu, sigma_init = measurement_update(mu, sigma_init, measurement, measurement_sigma)
+                    params['mu'], params['sigma'] = measurement_update(params['mu'], params['sigma'], measurement, params['measurement_sigma'])
                     plot_color = 'blue'
+                print(f'mu = {params["mu"]} truth = {robot_x}')
+
             if event.type == pg.MOUSEBUTTONDOWN:
-                for box in input_boxes:
+                for box in input_boxes.values():
                     box.handle_event(event)
+
             elif event.type == pg.KEYDOWN:
-                for box in input_boxes:
+                for box in input_boxes.values():
                     if box.active:
-                        box.handle_event(event)
-            for box in input_boxes:
+                        params[box.name] = box.handle_event(event)
+            for box in input_boxes.values():
                 box.draw()
         pg.display.flip()
 
 
-def drawgrid2():
+def drawgrid():
+    # screen.fill('black')
     robot = pg.image.load('robot2.png')
     robot = pg.transform.smoothscale(robot, (robot_size, robot_size))
     y = plot_height
@@ -99,14 +105,14 @@ def drawgrid2():
 
     screen.blit(robot, [robot_x-robot_size/2, y + cell_width/8])
 
-    gauss = gauss2surface(mu, sigma_init)
+    gauss = gauss2surface(params['mu'], params['sigma'])
     gauss = pg.transform.smoothscale(gauss, (plot_width-outline, plot_height-outline))
     screen.blit(gauss, [panel_width+outline, outline, win_width-outline, plot_height])
     plt.close()
 
 
-def motion_update(mu, sigma, action):
-    new_mu = mu + action
+def motion_update(mu, sigma, motion, motion_sigma):
+    new_mu = mu + motion
     new_sigma = np.sqrt(sigma**2 + motion_sigma**2)
     return new_mu, new_sigma
 
@@ -139,36 +145,27 @@ def gauss2surface(mu, sigma):
 
 class InputBox:
     #todo: fix numbers displayed outside of input box
-    def __init__(self, x, y, screen, name=''):
-        global sigma_init, motion_sigma, measurement_sigma, step_size
-        self.rect = pg.Rect(x+label_width, y, input_width, input_height)
+    def __init__(self, x, y, screen, name, value, font):
+        self.rect = pg.Rect(x+label_width, y, input_width+10, input_height)
         self.color = (200, 200, 200)
         self.color_active = pg.Color('dodgerblue2')
         self.color_inactive = 'white'
         self.text = ''
         self.name = name
+        self.value = value
         self.font = font
         self.txt_surface = self.font.render('', 1, 'white')
-        match self.name:
-            case 'sigma_init':
-                p = sigma_init
-            case 'motion_sigma':
-                p = motion_sigma
-            case 'measurement_sigma':
-                p = measurement_sigma
-            case 'step_size':
-                p = step_size
-        self.txt_surface = self.font.render(str(round(p, 2)), 1, self.color_inactive)
+        self.txt_surface = self.font.render(str(round(self.value, 2)), 1, self.color_inactive)
 
         self.active = False
         self.screen = screen
 
         label_box1 = pg.Rect(x+2, y, label_width, label_height)
-        label = self.font.render(f"{name}:          ", 1, 'white')
+        label = self.font.render(f"{self.name}:          ", 1, 'white')
         self.screen.blit(label, label_box1)
 
     def handle_event(self, event):
-        global sigma_init, motion_sigma, measurement_sigma, step_size
+        # global sigma, motion_sigma, measurement_sigma, step_size
         if event.type == pg.MOUSEBUTTONDOWN:
             # If the user clicked on the input_box rect.
             if self.rect.collidepoint(event.pos):
@@ -181,24 +178,16 @@ class InputBox:
         if event.type == pg.KEYDOWN:
             if self.active:
                 if event.key == pg.K_RETURN:
-                    match self.name:
-                        case 'sigma_init':
-                            sigma_init = float(self.text)
-                        case 'motion_sigma':
-                            motion_sigma = float(self.text)
-                        case 'measurement_sigma':
-                            measurement_sigma = float(self.text)
-                        case 'step_size':
-                            step_size = float(self.text)
+                    self.value = float(self.text)
                     self.active = False
                     self.color = self.color_inactive
                 elif event.key == pg.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
-
                 # Re-render the text.
                 self.txt_surface = self.font.render(self.text, True, 'white')
+        return self.value
 
     def draw(self):
         # delete old render before rerendering to avoid text overlap
@@ -207,5 +196,6 @@ class InputBox:
         self.screen.blit(self.txt_surface, (self.rect.x+2, self.rect.y))
         # Blit the rect.
         pg.draw.rect(self.screen, self.color, self.rect, 1)
-# print(measurement_update(10, 2, 12, 2))
+
+
 main()
---
+++ sandbox.py
@@ -0,0 +1,27 @@
+class sam:
+    def __init__(self, name):
+
+        self.name = name
+
+age = 28
+wage = 17
+params = [age, wage]
+age2 = params[0]
+age2 = 29
+print(age)
+
+
+# s = sam(name)
+#
+# print(s.name)
+# s.name = 'pedro'
+# print(name)
+
+# def f():
+#     global name
+#     b = {1: name}
+#     b[1] = 'pedro'
+#
+#     print(name)
+#
+# f()
