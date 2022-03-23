2597aa0da73a9b542b5f01632bf9a308708f6f6a
Samuel Rodriguez <samrolopez@gmail.com>
2022-03-23 11:03:51 +0100

    add horizontal motion

--- 2D_Kalman_filter.py
+++ 2D_Kalman_filter.py
@@ -8,6 +8,7 @@ from scipy.stats import multivariate_normal
 
 import utils
 
+
 def get_dpi():
     import sys
     from PyQt5.QtWidgets import QApplication
@@ -16,17 +17,36 @@ def get_dpi():
     dpi = screen.physicalDotsPerInch()
     app.quit()
     return dpi
-
 dpi =  get_dpi()
+
 class Robot:
-    def __init__(self, true_position, position_uncertainty, size):
+    def __init__(self, true_position, position_uncertainty, motion_uncertainty, size):
         self.pos = true_position
         self.mu = true_position
         self.sigma = position_uncertainty
+        self.motion_sigma = motion_uncertainty
         self.size = size
+        # self.world_size = world_size
+
+    def move(self, motion):
+        # motion update. Predict new position and variance.
+        if motion[0]:
+            self.mu[0] += motion[0]
+            self.sigma[0][0] = np.sqrt(self.sigma[0][0]**2 + self.motion_sigma**2)
+            self.pos[0] += np.random.normal(motion[0], self.motion_sigma, 1)[0]
+        elif motion[1]:
+            self.mu[1] += motion[1]
+            self.sigma[1][1] = np.sqrt(self.sigma[1][1]**2 + self.motion_sigma**2)
+            self.pos[1] += np.random.normal(motion[1], self.motion_sigma, 1)[0]
+
+
+    def bound_position(self, position):
+        #todo: bound movements to grid limits
+        return position
+
 
 class Kalman_2D:
-    def __init__(self, screen_width=2000, font_size=0):
+    def __init__(self, screen_width=1500, font_size=0):
         self.screen_width = screen_width
         self.screen_height = screen_width//2
         input_width = font_size * 2
@@ -34,19 +54,25 @@ class Kalman_2D:
         label_width = font_size * 8
         label_height = font_size * 1.5
         self.dpi = dpi
-
+        self.robot_size = self.screen_width//30
+        self.step_size = 10
         self.panel_width = input_width + label_width
+        self.cell_width = self.robot_size*1.2
+        self.robot_img = pg.transform.smoothscale(pg.image.load('robot2.png'), (self.robot_size, self.robot_size))
 
         self.plot_width = self.screen_width - self.panel_width
         self.plot_height = self.screen_height
+        self.figsize = (int(self.plot_width/self.dpi), int(self.plot_height/self.dpi))
         self.font_size = font_size
         self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
-        robot_x = random.randint(0, self.screen_width-1)
-        robot_y = random.randint(0, self.screen_height-1)
+        robot_x = random.randint(self.robot_size, self.screen_width-self.robot_size)
+        robot_y = random.randint(self.robot_size, self.screen_height-self.robot_size)
         mu = [robot_x, robot_y]
-        sigma = [[(self.screen_width+self.panel_width)*40, 2.], [2., self.screen_height*40]]
-        self.robot = Robot(mu, sigma, size=self.screen_width/20)
-        self.cell_width = self.robot.size*1.2
+        # start with big uncertainty
+        # sigma = [[(self.screen_width+self.panel_width)*40, 2.], [2., self.screen_height*40]]
+        sigma = [[100, 0], [0, 100]]
+        motion_sigma = 20.
+        self.robot = Robot(mu, sigma, motion_uncertainty=motion_sigma, size=self.robot_size)
         self.outline = 2
 
     def start(self):
@@ -60,29 +86,39 @@ class Kalman_2D:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_LEFT:
+                        motion = (-1 * self.step_size, 0)
+                    elif event.key == pg.K_RIGHT:
+                        motion = (1 * self.step_size, 0)
+                    else:
+                        motion = False
+                    if motion:
+                        self.robot.move(motion)
+                        print(f'sigmax= {self.robot.sigma[0][0]} sigmay={self.robot.sigma[1][1]}')
+                    if event.key == pg.K_m:
+                        self.robot.measure()
             pg.display.flip()
 
     def drawgrid(self):
-        robot = pg.image.load('robot2.png')
-        robot = pg.transform.smoothscale(robot, (self.robot.size, self.robot.size))
+
         rect = pg.Rect(self.panel_width, 0, self.screen_width, self.screen_height)
         pg.draw.rect(self.screen, (200, 200, 200), rect, 0)
 
         gauss = self.gauss2surface(self.robot.pos, self.robot.sigma)
         gauss = pg.transform.smoothscale(gauss, (self.plot_width, self.plot_height))
         self.screen.blit(gauss, (self.panel_width, 0))
-        self.screen.blit(robot, [(self.panel_width+self.robot.pos[0])-self.robot.size//2, self.robot.pos[1]-(self.robot.size//2)])
+        self.screen.blit(self.robot_img, [(self.panel_width+self.robot.pos[0])-self.robot.size//2, self.robot.pos[1]-(self.robot.size//2)])
 
         plt.close()
 
     def gauss2surface(self, mu, sigma):
-        scale = 10
-        x, y = np.mgrid[0:self.screen_width//scale:1/scale, self.screen_height//scale:0:-1/scale]
+        scale = 100
+        x, y = np.mgrid[0:self.screen_width//scale:1, self.screen_height//scale:0:-1]
         pos = np.dstack((x, y))
         rv = multivariate_normal(np.array(mu)/scale, np.array(sigma)/scale)
         z = rv.pdf(pos)
-        s = (int(self.plot_width/self.dpi), int(self.plot_height/self.dpi))
-        plt.figure(figsize=s)
+        plt.figure(figsize=self.figsize)
         fig = plt.gcf()
         ax = fig.add_axes([0, 0, 1, 1])
         ax.invert_yaxis()
@@ -100,8 +136,6 @@ class Kalman_2D:
 
         # And a corresponding grid
         ax.grid(which='both', c='k', ls='-', alpha=0.3)
-
-
         return utils.fig2surface(fig)
 
 
