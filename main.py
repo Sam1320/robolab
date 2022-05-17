be1e1eca19c7e5b655bed54236f788c59c1d3cea
Samuel Rodriguez <samrolopez@gmail.com>
2022-05-17 17:34:11 +0200

    add back button to part filter menu and set parameters of a* menu

--- main.py
+++ main.py
@@ -3,30 +3,48 @@ import pygame_menu
 import motion_planning
 import particle_filter
 
+#TODO:
+# improve buttons positions
+
 
 class AStarMenu(pygame_menu.Menu):
     def __init__(self, name, width=600, height=400, theme=pygame_menu.themes.THEME_SOLARIZED):
-        super().__init__(name, width, height, theme=theme)
-        self.add.range_slider('Grid Width :', default=10, range_values=list(range(10, 51, 10)))
-        self.add.range_slider('Grid Height :', default=10, range_values=list(range(10, 51, 10)))
+        super().__init__(name, width, height, theme=theme, onclose=pygame_menu.events.BACK)
+        self.add.range_slider('Grid Width :', default=10, range_values=list(range(10, 51, 10)), onchange=self.set_width)
+        self.add.range_slider('Grid Height :', default=10, range_values=list(range(10, 51, 10)), onchange=self.set_height)
         self.add.button('Ok', self.start_the_game)
+        self.add.button('Back', self.go_back)
+        self.width = 10
+        self.height = 10
+    def set_width(self, value):
+        self.width = value
+
+    def set_height(self, value):
+        self.height = value
+
+    def go_back(self):
+        self.close()
 
     def start(self, surface):
         self.mainloop(surface)
 
     def start_the_game(self):
-        self.gui = motion_planning.MotionPlanningGUI()
+        self.gui = motion_planning.MotionPlanningGUI(world_size=(self.width, self.height))
         self.gui.start()
         pygame.display.set_mode((600, 400))
 
 
 class ParticleMenu(pygame_menu.Menu):
     def __init__(self, name, width=600, height=400, theme=pygame_menu.themes.THEME_SOLARIZED):
-        super().__init__(name, width, height, theme=theme)
+        super().__init__(name, width, height, theme=theme, onclose=pygame_menu.events.BACK)
         self.add.range_slider('Number of Particles:', default=200, range_values=[10, 100, 200, 400, 800], onchange=self.set_number_of_particles)
         self.add.button('Ok', self.start_the_game)
+        self.add.button('Back', self.go_back)
+
         self.number_of_particles = 200
 
+    def go_back(self):
+        self.close()
     def start(self, surface):
         self.mainloop(surface)
 
